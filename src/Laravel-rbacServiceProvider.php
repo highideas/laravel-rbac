c40d481c03e1916f23eb81e1867c5b19ed29b4c8
Marco Araujo <marco.araujo.junior@gmail.com>
2015-10-21 00:39:35 -0200

    first commit

---
+++ src/Laravel-rbacServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Highideas\Laravel-rbac;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel-rbacServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
